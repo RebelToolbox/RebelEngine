5399c32b6c12897a711bbf16793a9be8826732cc
Marcel Admiraal <madmiraal@users.noreply.github.com>
2024-02-04 13:13:29 +0000

    Use the Rebel Engine image in the RebelEngine README.md

--- tools/scripts/make_icons.sh
+++ tools/scripts/make_icons.sh
@@ -6,7 +6,7 @@
 for s in 16 24 32 48 64 128 256 512 1024; do
   convert -resize ${s}x$s -antialias \
           -background transparent \
-          ../../rebel-logo.svg icon$s.png
+          ../images/rebel-engine-logo.svg icon$s.png
 done
 
 # 16px tga file for library
