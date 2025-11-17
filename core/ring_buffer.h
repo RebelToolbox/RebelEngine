3bec59f66e3ef10191e7b7b92007451766a84719
Marcel Admiraal <madmiraal@users.noreply.github.com>
2025-11-17 16:57:43 +0100

    Remove template id in constructors and destructors

--- core/ring_buffer.h
+++ core/ring_buffer.h
@@ -188,13 +188,13 @@ public:
         size_mask = mask;
     };
 
-    RingBuffer<T>(int p_power = 0) {
+    RingBuffer(int p_power = 0) {
         read_pos  = 0;
         write_pos = 0;
         resize(p_power);
     };
 
-    ~RingBuffer<T>(){};
+    ~RingBuffer(){};
 };
 
 #endif
