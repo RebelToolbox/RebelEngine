667b4eeac4f0c2c481c76747db091c1b483a5bf2
Marcel Admiraal <madmiraal@users.noreply.github.com>
2025-12-10 19:10:24 +0100

    Clean up NoiseTexture

--- modules/opensimplex/noise_texture.cpp
+++ modules/opensimplex/noise_texture.cpp
@@ -9,26 +9,143 @@
 #include "core/core_string_names.h"
 
 NoiseTexture::NoiseTexture() {
-    update_queued = false;
-    regen_queued  = false;
-    first_time    = true;
-
-    size          = Vector2i(512, 512);
-    seamless      = false;
-    as_normalmap  = false;
-    bump_strength = 8.0;
-    flags         = FLAGS_DEFAULT;
-
-    noise = Ref<OpenSimplexNoise>();
-
     texture = VS::get_singleton()->texture_create();
-
     _queue_update();
 }
 
 NoiseTexture::~NoiseTexture() {
     VS::get_singleton()->free(texture);
     noise_thread.wait_to_finish();
+}
+
+int NoiseTexture::get_width() const {
+    return size.x;
+}
+
+int NoiseTexture::get_height() const {
+    return size.y;
+}
+
+uint32_t NoiseTexture::get_flags() const {
+    return flags;
+}
+
+void NoiseTexture::set_flags(const uint32_t new_flags) {
+    flags = new_flags;
+    VS::get_singleton()->texture_set_flags(texture, flags);
+}
+
+RID NoiseTexture::get_rid() const {
+    return texture;
+}
+
+bool NoiseTexture::has_alpha() const {
+    return false;
+}
+
+Ref<Image> NoiseTexture::get_data() const {
+    return data;
+}
+
+void NoiseTexture::set_width(const int new_width) {
+    if (size.x == new_width) {
+        return;
+    }
+    size.x = new_width;
+    _queue_update();
+}
+
+void NoiseTexture::set_height(const int new_height) {
+    if (size.y == new_height) {
+        return;
+    }
+    size.y = new_height;
+    _queue_update();
+}
+
+Ref<OpenSimplexNoise> NoiseTexture::get_noise() {
+    return noise;
+}
+
+void NoiseTexture::set_noise(Ref<OpenSimplexNoise> new_noise) {
+    if (noise == new_noise) {
+        return;
+    }
+    if (noise.is_valid()) {
+        noise->disconnect(
+            CoreStringNames::get_singleton()->changed,
+            this,
+            "_queue_update"
+        );
+    }
+    noise = new_noise;
+    if (noise.is_valid()) {
+        noise->connect(
+            CoreStringNames::get_singleton()->changed,
+            this,
+            "_queue_update"
+        );
+    }
+    _queue_update();
+}
+
+Vector2 NoiseTexture::get_noise_offset() const {
+    return noise_offset;
+}
+
+void NoiseTexture::set_noise_offset(const Vector2 new_noise_offset) {
+    if (noise_offset == new_noise_offset) {
+        return;
+    }
+    noise_offset = new_noise_offset;
+    _queue_update();
+}
+
+bool NoiseTexture::get_seamless() const {
+    return seamless;
+}
+
+void NoiseTexture::set_seamless(const bool new_seamless) {
+    if (seamless == new_seamless) {
+        return;
+    }
+    seamless = new_seamless;
+    _queue_update();
+}
+
+bool NoiseTexture::is_normalmap() const {
+    return normalmap;
+}
+
+void NoiseTexture::set_as_normalmap(const bool new_normalmap) {
+    if (new_normalmap == normalmap) {
+        return;
+    }
+    normalmap = new_normalmap;
+    _queue_update();
+    _change_notify();
+}
+
+float NoiseTexture::get_bump_strength() const {
+    return bump_strength;
+}
+
+void NoiseTexture::set_bump_strength(const float new_bump_strength) {
+    if (bump_strength == new_bump_strength) {
+        return;
+    }
+    bump_strength = new_bump_strength;
+    if (normalmap) {
+        _queue_update();
+    }
+}
+
+void NoiseTexture::_validate_property(PropertyInfo& property) const {
+    if (property.name == "bump_strength") {
+        if (!normalmap) {
+            property.usage = PROPERTY_USAGE_NOEDITOR | PROPERTY_USAGE_INTERNAL;
+        }
+    }
 }
 
 void NoiseTexture::_bind_methods() {
@@ -40,58 +157,53 @@ void NoiseTexture::_bind_methods() {
         D_METHOD("set_height", "height"),
         &NoiseTexture::set_height
     );
-
+    ClassDB::bind_method(D_METHOD("get_noise"), &NoiseTexture::get_noise);
     ClassDB::bind_method(
         D_METHOD("set_noise", "noise"),
         &NoiseTexture::set_noise
-    );
-    ClassDB::bind_method(D_METHOD("get_noise"), &NoiseTexture::get_noise);
-
-    ClassDB::bind_method(
-        D_METHOD("set_noise_offset", "noise_offset"),
-        &NoiseTexture::set_noise_offset
     );
     ClassDB::bind_method(
         D_METHOD("get_noise_offset"),
         &NoiseTexture::get_noise_offset
     );
-
+    ClassDB::bind_method(
+        D_METHOD("set_noise_offset", "noise_offset"),
+        &NoiseTexture::set_noise_offset
+    );
+    ClassDB::bind_method(D_METHOD("get_seamless"), &NoiseTexture::get_seamless);
     ClassDB::bind_method(
         D_METHOD("set_seamless", "seamless"),
         &NoiseTexture::set_seamless
     );
-    ClassDB::bind_method(D_METHOD("get_seamless"), &NoiseTexture::get_seamless);
-
+    ClassDB::bind_method(D_METHOD("is_normalmap"), &NoiseTexture::is_normalmap);
     ClassDB::bind_method(
         D_METHOD("set_as_normalmap", "as_normalmap"),
         &NoiseTexture::set_as_normalmap
-    );
-    ClassDB::bind_method(D_METHOD("is_normalmap"), &NoiseTexture::is_normalmap);
-
-    ClassDB::bind_method(
-        D_METHOD("set_bump_strength", "bump_strength"),
-        &NoiseTexture::set_bump_strength
     );
     ClassDB::bind_method(
         D_METHOD("get_bump_strength"),
         &NoiseTexture::get_bump_strength
     );
+    ClassDB::bind_method(
+        D_METHOD("set_bump_strength", "bump_strength"),
+        &NoiseTexture::set_bump_strength
+    );
 
     ClassDB::bind_method(
-        D_METHOD("_update_texture"),
-        &NoiseTexture::_update_texture
+        D_METHOD("_generate_texture"),
+        &NoiseTexture::_generate_texture
     );
     ClassDB::bind_method(
         D_METHOD("_queue_update"),
         &NoiseTexture::_queue_update
     );
     ClassDB::bind_method(
-        D_METHOD("_generate_texture"),
-        &NoiseTexture::_generate_texture
-    );
-    ClassDB::bind_method(
         D_METHOD("_thread_done", "image"),
         &NoiseTexture::_thread_done
+    );
+    ClassDB::bind_method(
+        D_METHOD("_update_texture"),
+        &NoiseTexture::_update_texture
     );
 
     ADD_PROPERTY(
@@ -115,6 +227,21 @@ void NoiseTexture::_bind_methods() {
         "get_height"
     );
     ADD_PROPERTY(
+        PropertyInfo(
+            Variant::OBJECT,
+            "noise",
+            PROPERTY_HINT_RESOURCE_TYPE,
+            "OpenSimplexNoise"
+        ),
+        "set_noise",
+        "get_noise"
+    );
+    ADD_PROPERTY(
+        PropertyInfo(Variant::VECTOR2, "noise_offset"),
+        "set_noise_offset",
+        "get_noise_offset"
+    );
+    ADD_PROPERTY(
         PropertyInfo(Variant::BOOL, "seamless"),
         "set_seamless",
         "get_seamless"
@@ -134,92 +261,14 @@ void NoiseTexture::_bind_methods() {
         "set_bump_strength",
         "get_bump_strength"
     );
-    ADD_PROPERTY(
-        PropertyInfo(
-            Variant::OBJECT,
-            "noise",
-            PROPERTY_HINT_RESOURCE_TYPE,
-            "OpenSimplexNoise"
-        ),
-        "set_noise",
-        "get_noise"
-    );
-    ADD_PROPERTY(
-        PropertyInfo(Variant::VECTOR2, "noise_offset"),
-        "set_noise_offset",
-        "get_noise_offset"
-    );
-}
-
-void NoiseTexture::_validate_property(PropertyInfo& property) const {
-    if (property.name == "bump_strength") {
-        if (!as_normalmap) {
-            property.usage = PROPERTY_USAGE_NOEDITOR | PROPERTY_USAGE_INTERNAL;
-        }
-    }
-}
-
-void NoiseTexture::_set_texture_data(const Ref<Image>& p_image) {
-    data = p_image;
-    if (data.is_valid()) {
-        VS::get_singleton()->texture_allocate(
-            texture,
-            size.x,
-            size.y,
-            0,
-            p_image->get_format(),
-            VS::TEXTURE_TYPE_2D,
-            flags
-        );
-        VS::get_singleton()->texture_set_data(texture, p_image);
-    }
-    emit_changed();
-}
-
-void NoiseTexture::_thread_done(const Ref<Image>& p_image) {
-    _set_texture_data(p_image);
-    noise_thread.wait_to_finish();
-    if (regen_queued) {
-        noise_thread.start(_thread_function, this);
-        regen_queued = false;
-    }
-}
-
-void NoiseTexture::_thread_function(void* p_ud) {
-    NoiseTexture* tex = (NoiseTexture*)p_ud;
-    tex->call_deferred("_thread_done", tex->_generate_texture());
 }
 
 void NoiseTexture::_queue_update() {
     if (update_queued) {
         return;
     }
-
     update_queued = true;
     call_deferred("_update_texture");
-}
-
-Ref<Image> NoiseTexture::_generate_texture() {
-    // Prevent memdelete due to unref() on other thread.
-    Ref<OpenSimplexNoise> ref_noise = noise;
-
-    if (ref_noise.is_null()) {
-        return Ref<Image>();
-    }
-
-    Ref<Image> image;
-
-    if (seamless) {
-        image = ref_noise->get_seamless_image(size.x);
-    } else {
-        image = ref_noise->get_image(size.x, size.y, noise_offset);
-    }
-
-    if (as_normalmap) {
-        image->bumpmap_to_normalmap(bump_strength);
-    }
-
-    return image;
 }
 
 void NoiseTexture::_update_texture() {
@@ -234,128 +283,64 @@ void NoiseTexture::_update_texture() {
     if (use_thread) {
         if (!noise_thread.is_started()) {
             noise_thread.start(_thread_function, this);
-            regen_queued = false;
+            regeneration_queued = false;
         } else {
-            regen_queued = true;
+            regeneration_queued = true;
         }
-
     } else {
-        Ref<Image> image = _generate_texture();
+        const Ref<Image> image = _generate_texture();
         _set_texture_data(image);
     }
     update_queued = false;
 }
 
-void NoiseTexture::set_noise(Ref<OpenSimplexNoise> p_noise) {
-    if (p_noise == noise) {
-        return;
+Ref<Image> NoiseTexture::_generate_texture() const {
+    // Prevent memdelete due to unref() on other thread.
+    Ref<OpenSimplexNoise> noise_copy = noise;
+    if (noise_copy.is_null()) {
+        return {};
     }
-    if (noise.is_valid()) {
-        noise->disconnect(
-            CoreStringNames::get_singleton()->changed,
-            this,
-            "_queue_update"
+    Ref<Image> image;
+    if (seamless) {
+        image = noise_copy->get_seamless_image(size.x);
+    } else {
+        image = noise_copy->get_image(size.x, size.y, noise_offset);
+    }
+    if (normalmap) {
+        image->bumpmap_to_normalmap(bump_strength);
+    }
+    return image;
+}
+
+void NoiseTexture::_thread_done(const Ref<Image>& image) {
+    _set_texture_data(image);
+    noise_thread.wait_to_finish();
+    if (regeneration_queued) {
+        noise_thread.start(_thread_function, this);
+        regeneration_queued = false;
+    }
+}
+
+void NoiseTexture::_set_texture_data(const Ref<Image>& image) {
+    if (image.is_valid()) {
+        VS::get_singleton()->texture_allocate(
+            texture,
+            size.x,
+            size.y,
+            0,
+            image->get_format(),
+            VS::TEXTURE_TYPE_2D,
+            flags
         );
+        VS::get_singleton()->texture_set_data(texture, image);
     }
-    noise = p_noise;
-    if (noise.is_valid()) {
-        noise->connect(
-            CoreStringNames::get_singleton()->changed,
-            this,
-            "_queue_update"
-        );
-    }
-    _queue_update();
+    emit_changed();
 }
 
-Ref<OpenSimplexNoise> NoiseTexture::get_noise() {
-    return noise;
-}
-
-void NoiseTexture::set_width(int p_width) {
-    if (p_width == size.x) {
-        return;
-    }
-    size.x = p_width;
-    _queue_update();
-}
-
-void NoiseTexture::set_height(int p_height) {
-    if (p_height == size.y) {
-        return;
-    }
-    size.y = p_height;
-    _queue_update();
-}
-
-void NoiseTexture::set_noise_offset(Vector2 p_noise_offset) {
-    if (noise_offset == p_noise_offset) {
-        return;
-    }
-    noise_offset = p_noise_offset;
-    _queue_update();
-}
-
-void NoiseTexture::set_seamless(bool p_seamless) {
-    if (p_seamless == seamless) {
-        return;
-    }
-    seamless = p_seamless;
-    _queue_update();
-}
-
-bool NoiseTexture::get_seamless() {
-    return seamless;
-}
-
-void NoiseTexture::set_as_normalmap(bool p_as_normalmap) {
-    if (p_as_normalmap == as_normalmap) {
-        return;
-    }
-    as_normalmap = p_as_normalmap;
-    _queue_update();
-    _change_notify();
-}
-
-bool NoiseTexture::is_normalmap() {
-    return as_normalmap;
-}
-
-void NoiseTexture::set_bump_strength(float p_bump_strength) {
-    if (p_bump_strength == bump_strength) {
-        return;
-    }
-    bump_strength = p_bump_strength;
-    if (as_normalmap) {
-        _queue_update();
-    }
-}
-
-float NoiseTexture::get_bump_strength() {
-    return bump_strength;
-}
-
-int NoiseTexture::get_width() const {
-    return size.x;
-}
-
-int NoiseTexture::get_height() const {
-    return size.y;
-}
-
-Vector2 NoiseTexture::get_noise_offset() const {
-    return noise_offset;
-}
-
-void NoiseTexture::set_flags(uint32_t p_flags) {
-    flags = p_flags;
-    VS::get_singleton()->texture_set_flags(texture, flags);
-}
-
-uint32_t NoiseTexture::get_flags() const {
-    return flags;
-}
-
-Ref<Image> NoiseTexture::get_data() const {
-    return data;
+void NoiseTexture::_thread_function(void* user_data) {
+    const auto noise_texture = static_cast<NoiseTexture*>(user_data);
+    noise_texture->call_deferred(
+        "_thread_done",
+        noise_texture->_generate_texture()
+    );
 }
--- modules/opensimplex/noise_texture.h
+++ modules/opensimplex/noise_texture.h
@@ -9,82 +9,68 @@
 
 #include "core/image.h"
 #include "core/reference.h"
-#include "editor/editor_node.h"
-#include "editor/editor_plugin.h"
-#include "editor/property_editor.h"
+#include "core/rid.h"
 #include "open_simplex_noise.h"
+#include "scene/resources/texture.h"
 
 class NoiseTexture : public Texture {
     GDCLASS(NoiseTexture, Texture);
 
-private:
-    Ref<Image> data;
-
-    Thread noise_thread;
-
-    bool first_time;
-    bool update_queued;
-    bool regen_queued;
-
-    RID texture;
-    uint32_t flags;
-
-    Ref<OpenSimplexNoise> noise;
-    Vector2i size;
-    Vector2 noise_offset;
-    bool seamless;
-    bool as_normalmap;
-    float bump_strength;
-
-    void _thread_done(const Ref<Image>& p_image);
-    static void _thread_function(void* p_ud);
-
-    void _queue_update();
-    Ref<Image> _generate_texture();
-    void _update_texture();
-    void _set_texture_data(const Ref<Image>& p_image);
-
-protected:
-    static void _bind_methods();
-    virtual void _validate_property(PropertyInfo& property) const;
-
 public:
-    void set_noise(Ref<OpenSimplexNoise> p_noise);
-    Ref<OpenSimplexNoise> get_noise();
-
-    void set_width(int p_width);
-    void set_height(int p_height);
-
-    void set_noise_offset(Vector2 p_noise_offset);
-    Vector2 get_noise_offset() const;
-
-    void set_seamless(bool p_seamless);
-    bool get_seamless();
-
-    void set_as_normalmap(bool p_as_normalmap);
-    bool is_normalmap();
-
-    void set_bump_strength(float p_bump_strength);
-    float get_bump_strength();
-
-    int get_width() const;
-    int get_height() const;
-
-    virtual void set_flags(uint32_t p_flags);
-    virtual uint32_t get_flags() const;
-
-    virtual RID get_rid() const {
-        return texture;
-    }
-
-    virtual bool has_alpha() const {
-        return false;
-    }
-
-    virtual Ref<Image> get_data() const;
-
     NoiseTexture();
     virtual ~NoiseTexture();
+
+    virtual int get_width() const;
+    virtual int get_height() const;
+    virtual uint32_t get_flags() const;
+    virtual void set_flags(uint32_t new_flags);
+    virtual RID get_rid() const;
+    virtual bool has_alpha() const;
+    virtual Ref<Image> get_data() const;
+
+    void set_width(int new_width);
+    void set_height(int new_height);
+    Ref<OpenSimplexNoise> get_noise();
+    void set_noise(Ref<OpenSimplexNoise> new_noise);
+    Vector2 get_noise_offset() const;
+    void set_noise_offset(Vector2 new_noise_offset);
+    bool get_seamless() const;
+    void set_seamless(bool new_seamless);
+    bool is_normalmap() const;
+    void set_as_normalmap(bool new_normalmap);
+    float get_bump_strength() const;
+    void set_bump_strength(float new_bump_strength);
+
+protected:
+    virtual void _validate_property(PropertyInfo& property) const;
+
+    static void _bind_methods();
+
+private:
+    Thread noise_thread;
+
+    RID texture;
+    Ref<Image> data;
+    Ref<OpenSimplexNoise> noise;
+
+    Vector2i size{512, 512};
+    Vector2 noise_offset;
+
+    uint32_t flags           = FLAGS_DEFAULT;
+    float bump_strength      = 8;
+    bool first_time          = true;
+    bool regeneration_queued = false;
+    bool update_queued       = false;
+    bool normalmap           = false;
+    bool seamless            = false;
+
+    void _queue_update();
+    void _update_texture();
+    Ref<Image> _generate_texture() const;
+    void _thread_done(const Ref<Image>& image);
+    void _set_texture_data(const Ref<Image>& image);
+
+    static void _thread_function(void* user_data);
 };
 
 #endif // NOISE_TEXTURE_H
